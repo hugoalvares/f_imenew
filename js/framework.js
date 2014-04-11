82b37a6ba2142b091df2bca226facff873c9335c
Hugo Alvares <hugoalvares@outlook.com>
2014-04-11 20:50:39 -0300

    telas novas

--- js/framework.js
+++ js/framework.js
@@ -4,6 +4,11 @@ var js = {
     },
     mapaClick : function() {
         window.location.href = 'cardapio.html';
+    },
+    produtoClick : function() {
+        window.location.href = 'detalhe.html';
+    },
+    adicionar : function() {
+    	window.location.href = 'cardapio.html';
     }
-    
 }
